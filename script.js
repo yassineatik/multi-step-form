56d168bcb7a59dfbd49999416903f561ec5af7de
Yassine Atik <brucexx02@gmail.com>
2022-12-10 03:09:22 +0100

    Designed Multi Step Form With HTML/CSS & JS

---
+++ script.js
@@ -0,0 +1,48 @@
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById("progress");
+const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step");
+let formStepsNum = 0;
+
+function updateFormSteps() {
+
+    formSteps.forEach(formStep => {
+        formStep.classList.contains("active") &&
+            formStep.classList.remove("active");
+    })
+    formSteps[formStepsNum].classList.add("active");
+}
+
+function updateProgressBar() {
+    progressSteps.forEach((progressStep, idx) => {
+        if (idx < formStepsNum + 1) {
+            progressStep.classList.add("active");
+        } else {
+            progressStep.classList.remove("active");
+        }
+    })
+
+    const progressActive = document.querySelectorAll(".progress-step.active");
+    progress.style.width = ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + '%';
+}
+
+
+nextBtns.forEach(btn => {
+    btn.addEventListener("click", function () {
+        formStepsNum++;
+        updateFormSteps();
+        updateProgressBar();
+        console.log("kk")
+    })
+})
+
+
+prevBtns.forEach(btn => {
+    btn.addEventListener("click", function () {
+        formStepsNum--;
+        updateFormSteps();
+        updateProgressBar();
+        console.log("kk")
+    })
+})
